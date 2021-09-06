62fbd28dd0b70f822e41e986a57d31de7f9d9ab6
TatianeRosa27 <tatiane.rosa@estudante.iftm.edu.br>
2021-09-06 10:35:48 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import Navbar from "components/NavBAr";
 
 
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Ola Mundo!</h1>
-    </div>
+    <>
+      <Navbar />
+      <div className="container">
+        <h1 className="text-primary">Ola Mundo!</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
   );
 }
 
